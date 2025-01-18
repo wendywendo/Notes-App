8063be510ef6f20e64df8d4038ded3697777210b
wendywendo <wendywendo06@gmail.com>
2025-01-18 17:33:23 +0300

    Changed CORS origin

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -6,7 +6,7 @@ const { test, registerUser, loginUser, getProfile, logoutUser } = require('../co
 // Middleware
 router.use(cors({
     credentials: true,
-    origin: 'http://localhost:5173'
+    origin: 'https://notes-app-59at.vercel.app'
 }))
 
 router.get('/', test)
