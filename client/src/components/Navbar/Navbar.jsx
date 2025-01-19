ba098fa9685728b5ba127557fe1a5489e1060b81
wendywendo <wendywendo06@gmail.com>
2025-01-19 22:13:29 +0300

    Changed Logout feature

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,19 +1,21 @@
 import { useContext } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { UserContext } from '../../../context/UserContext'
 import axios from 'axios'
 import { ToastContainer, toast } from 'react-toastify';
 import './Navbar.css'
 
+
 export default function Navbar() {
 
   const { user } = useContext(UserContext)
+  const navigate = useNavigate()
 
   const logoutUser = async () => {
     await axios.post('/logout')
     toast('Successfully logged out!')
     setTimeout(() => {
-      window.location.href = '/login';
+      navigate('/login')
     }, 2000);
   }
 
